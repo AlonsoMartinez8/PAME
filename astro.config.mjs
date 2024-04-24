66baa9d3ee9db523c79d070863b7697f664cbbe0
pameproj <pameprojectmail@gmail.com>
2024-04-24 21:24:57 +0200

    Astro add React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   site: 'https://pameproj.github.io',
   base: '/PAME'
 });
