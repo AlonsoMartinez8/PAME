744326d71f360396ab53fecd310b067a9552ffe4
pameproj <pameprojectmail@gmail.com>
2024-04-24 23:44:56 +0200

    Change Header from Astro to JSX component

---
+++ src/components/Header.jsx
@@ -0,0 +1,64 @@
+import BtnLink from "@c/BtnLink.astro";
+import NavLink from "@c/NavLink.astro";
+import { useEffect, useState } from "react";
+// import PameLogo from "./PameLogo.astro";
+// <a href="/PAME" class="hover:animate-pulse"><PameLogo /></a>
+// <NavLink href={link.href} content={link.content} />
+// <BtnLink href="/" content="Log In" />
+
+export default function Header(headerLinks) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const handleClick = () => {
+    setSidebarOpen((open) => !open);
+  };
+
+  useEffect(()=>{
+    console.log(headerLinks)
+  },[])
+
+  return (
+    <header className="w-full sticky top-0 grid grid-cols-8 backdrop-blur-md items-center">
+      <aside className="col-span-4 sm:col-span-2 text-center p-4 flex items-center justify-start">
+        <a href="/PAME" className="hover:animate-pulse">
+          <h1 className="text-2xl font-semibold">P A M E</h1>
+        </a>
+      </aside>
+
+      <aside className="col-span-4 sm:col-span-6 p-4 flex items-center justify-end md:hidden">
+        <button onClick={handleClick}>
+          <i className="text-2xl ri-menu-3-line"></i>
+        </button>
+      </aside>
+
+      <section
+        id="sidebar"
+        className={`${
+          sidebarOpen ? "flex" : "hidden"
+        } absolute top-0 right-0 h-screen flex-col justify-between w-[80%] md:relative md:h-auto md:w-auto col-span-6 md:grid grid-cols-6 md:items-center bg-slate-600 md:bg-transparent`}
+      >
+        <header className="p-4 flex items-center justify-end md:hidden">
+          <aside>
+            <button onClick={handleClick}>
+              <i className="text-2xl ri-xrp-line"></i>
+            </button>
+          </aside>
+        </header>
+
+        <nav className="col-span-4 text-center p-4 h-full">
+          <ul className="flex flex-col items-end md:flex-row md:items-center md:justify-around py-1 gap-2">
+            {headerLinks && headerLinks.length > 0 ? (
+              headerLinks.map((link) => <li>
+                <a href={link.href}>{link.content}</a>
+              </li>)
+            ) : (
+              <li>Your Wardrobe Online</li>
+            )}
+          </ul>
+        </nav>
+
+        <aside className="col-span-2 text-center p-4 flex items-center justify-center md:justify-end"></aside>
+      </section>
+    </header>
+  );
+}
