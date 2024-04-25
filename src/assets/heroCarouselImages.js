bf2b01b55edc5436a089c130c9feaf7ff84b2753
pameproj <pameprojectmail@gmail.com>
2024-04-25 15:33:03 +0200

    Hero Carousel Images

---
+++ src/assets/heroCarouselImages.js
@@ -0,0 +1,18 @@
+export const images = [
+    {
+        id: 1,
+        img: "src/assets/img/3dWardrobeIllustration.png"
+    }, {
+        id: 2,
+        img: "src/assets/img/friendsIllustration.png"
+    }, {
+        id: 3,
+        img: "src/assets/img/outfitIllustration.png"
+    }, {
+        id: 4,
+        img: "src/assets/img/menLaptopIllustration.png"
+    }, {
+        id: 5,
+        img: "src/assets/img/womanMobileIllustration.png"
+    }
+]
