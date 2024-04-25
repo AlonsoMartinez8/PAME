c67d3ccddf2ba1ce9c5394389de7705307c8abc0
pameproj <pameprojectmail@gmail.com>
2024-04-25 12:47:48 +0200

    Header Links

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,7 +1,7 @@
 export default function ({ href, content }) {
   return (
     <li>
-      <a className="px-6 py-2 hover:animate-pulse" href={href}>
+      <a className="p-2 hover:animate-pulse" href={href}>
         {content}
       </a>
     </li>
