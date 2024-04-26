d7fa627caac87d5b18c1788669cb8b6374582a34
pameproj <pameprojectmail@gmail.com>
2024-04-26 13:51:34 +0200

    Create User and Session tables

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,23 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from "astro:db";
+
+const User = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true, optional: false, unique: true }),
+    username: column.text({ unique: true, optional: false }),
+    email: column.text({ optional: false }),
+    pasword: column.text({ optional: false }),
+  },
+});
+
+const Session = defineTable({
+  columns: {
+    id: column.text({  optional: false, unique: true }),
+    userId: column.text({ optional: false, references: ()=>User.columns.id }),
+    expiresAt: column.number({ optional: false }),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {User, Session},
 });
