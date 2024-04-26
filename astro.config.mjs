511d2336ea4fa73c3d5f60c8fe1f42670d935dc5
pameproj <pameprojectmail@gmail.com>
2024-04-26 12:39:04 +0200

    add Astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), db()],
   site: 'https://pameproj.github.io',
   base: '/PAME',
   output: "server",
