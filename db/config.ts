0136c6fe6cdb603a5f1c612618b2f2b5dc26e7da
pameproj <pameprojectmail@gmail.com>
2024-04-26 15:30:16 +0200

    Register config

--- db/config.ts
+++ db/config.ts
@@ -5,7 +5,7 @@ const User = defineTable({
     id: column.text({ primaryKey: true, optional: false, unique: true }),
     username: column.text({ unique: true, optional: false }),
     email: column.text({ optional: false }),
-    pasword: column.text({ optional: false }),
+    password: column.text({ optional: false }),
   },
 });
 
