cc607494d0e1c2eaca5b3f0916bacfbfcfa52dee
pameproj <pameprojectmail@gmail.com>
2024-04-29 20:38:40 +0200

    Add db and vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import db from "@astrojs/db";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), db()],
+  output: "server",
+  adapter: vercel()
 });
