6f678dfc6def05c59ed396207f058c8e526130ec
pameproj <pameprojectmail@gmail.com>
2024-04-29 20:21:19 +0200

    Remove db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import db from "@astrojs/db";
-
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-<<<<<<< HEAD
   integrations: [tailwind(), react()],
-=======
-  integrations: [tailwind(), react(), db()],
-  site: 'https://pameproj.github.io',
-  base: '/PAME',
-  output: "server",
-  adapter: vercel(),
->>>>>>> 48207568d33804e4e2ee43d468de569d6a85aad9
 });
