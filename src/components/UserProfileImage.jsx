bd33b384c51edfbc5faf3f9318a1398b41ad45e6
pameproj <pameprojectmail@gmail.com>
2024-04-30 18:46:26 +0200

    User Profile Image component functionality

--- src/components/UserProfileImage.jsx
+++ src/components/UserProfileImage.jsx
@@ -1,23 +1,59 @@
+import { useRef } from "react";
 import noUserImage from "../../public/img/noUserImage.png";
 
-export default function UserProfileImage({ dbUser }) {
+export default function UserProfileImage({ dbUser, width }) {
+  const formRef = useRef(null);
+
+  const handleImageClick = () => {
+    document.getElementById("fileInput").click();
+  };
+
+  const handleFileInputChange = (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      document.getElementById("fileForm").submit();
+    }
+    if (formRef.current) {
+      formRef.current.submit();
+    }
+  };
+
   return (
     <>
-      {dbUser && dbUser.imageUrl ? (
+      {dbUser.imageUrl && dbUser.imageUrl !== "" ? (
         <>
           <img
-            class="w-44 mx-auto bg-slate-100/50 aspect-square rounded-full m-5"
+            className="mx-auto bg-slate-100/50 aspect-square rounded-full m-5 cursor-pointer"
+            width={width}
             src={dbUser.imageUrl}
+            onClick={handleImageClick}
           />
         </>
       ) : (
         <>
           <img
-            class="w-44 mx-auto bg-slate-100/50 aspect-square rounded-full m-5"
+            className="mx-auto bg-slate-100/50 aspect-square rounded-full m-5 cursor-pointer"
+            width={width}
             src={noUserImage.src}
+            onClick={handleImageClick}
           />
         </>
       )}
+      <form
+        ref={formRef}
+        className="hidden"
+        id="fileForm"
+        action="api/updateImage"
+        method="post"
+      >
+        <input type="hidden" name="userId" value={dbUser.id} />
+        <input
+          type="file"
+          name="file"
+          id="fileInput"
+          onChange={handleFileInputChange}
+        />
+      </form>
     </>
   );
 }
