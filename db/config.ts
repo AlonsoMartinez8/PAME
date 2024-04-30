9696e6b76860b5d4b3ff50df68e43ae5879d0fb1
pameproj <pameprojectmail@gmail.com>
2024-04-30 12:07:10 +0200

    User new table

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,18 @@
 import { defineDb, defineTable, column } from "astro:db";
 
 const User = defineTable({
+  deprecated: true,
+  columns: {
+    id: column.text({ primaryKey: true, optional: false, unique: true }),
+    username: column.text({ optional: false, unique: true }),
+    password: column.text({ optional: false }),
+    imageUrl: column.text({optional: true}),
+    description: column.text({optional: true}),
+    link: column.text({optional: true}),
+  },
+});
+
+const UserNew = defineTable({
   columns: {
     id: column.text({ primaryKey: true, optional: false, unique: true }),
     username: column.text({ optional: false, unique: true }),
