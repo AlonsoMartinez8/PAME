02b56b565a22debcee3976104d2dec77ea54179d
pameproj <pameprojectmail@gmail.com>
2024-05-07 23:16:04 +0200

    Profile issue

--- src/components/UserProfileImage.jsx
+++ src/components/UserProfileImage.jsx
@@ -11,6 +11,7 @@ export default function UserProfileImage({ dbUser, width }) {
     document.getElementById("fileInput").click();
   };
 
+  // ISSUES -> Old images no need to be storaged
   const handleFileInputChange = async (event) => {
     const selectedFile = event.target.files[0];
     if (selectedFile) {
