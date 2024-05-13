414632ee2474e37024e19ff74016ad2f6f08df43
AlonsoMartinez8 <pameprojectmail@gmail.com>
2024-05-13 16:21:27 +0200

    Clothe card onClick

--- src/components/ClotheCard.jsx
+++ src/components/ClotheCard.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 export default function ClotheCard({ clothe }) {
+
+  const handleClick = () => {}; // Redirect to /clothe/[clotheId].astro
+
   return (
     <div
       key={clothe.id}
       className={`w-36 md:w-44 rounded-xl overflow-hidden cursor-pointer bg-slate-50/50`}
+      onClick={handleClick}
     >
       <img
         src={clothe.imageUrl}
+        alt={clothe.name}
         className="h-64 md:h-72 w-full pointer-events-none object-cover mx-auto"
       />
     </div>
