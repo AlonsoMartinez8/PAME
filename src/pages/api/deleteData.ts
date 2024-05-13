d4d1febf041da7f83ff522efe1510dab8d839ca7
BzRub <trabajofinalgradoruben@gmail.com>
2024-05-13 15:08:27 +0200

    Delete clothes

--- src/pages/api/deleteData.ts
+++ src/pages/api/deleteData.ts
@@ -12,14 +12,8 @@ import {
 } from "astro:db";
 
 export async function POST(context: APIContext): Promise<Response> {
-  await db.delete(Session);
-  await db.delete(User);
-  await db.delete(Wardrobe);
+
   await db.delete(Clothe);
-  await db.delete(Outfit);
-  await db.delete(Category);
-  await db.delete(Follow);
-  await db.delete(Like);
 
   return context.redirect("/");
 }
