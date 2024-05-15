50b7858e6bddf08d1bca6fbb95ff51c0ead2c66e
AlonsoMartinez8 <pameprojectmail@gmail.com>
2024-05-15 11:05:44 +0200

    View transitions

--- src/components/ClotheCard.jsx
+++ src/components/ClotheCard.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 
 export default function ClotheCard({ clothe, redi }) {
-  const handleClick = () => {
-    redi && (window.location.href = `/clothe/${clothe.id}`);
-  };
-
   return (
-    <div
+    <a
+      href={`/clothe/${clothe.id}`}
       key={clothe.id}
       className={`w-36 md:w-44 rounded-xl overflow-hidden cursor-pointer bg-slate-50/50`}
-      onClick={handleClick}
     >
       <img
+        transition:name={clothe.id}
         src={clothe.imageUrl}
         alt={clothe.name}
         className="h-64 md:h-72 w-full pointer-events-none object-cover mx-auto"
       />
-    </div>
+    </a>
   );
 }
