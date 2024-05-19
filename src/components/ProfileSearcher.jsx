9627fcc7bee6bb75d5c21b3271e918398ba8feb1
AlonsoMartinez8 <pameprojectmail@gmail.com>
2024-05-19 22:49:23 +0200

    Search functionality fixed

--- src/components/ProfileSearcher.jsx
+++ src/components/ProfileSearcher.jsx
@@ -1,73 +1,48 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import ProfileCard from "./ProfileCard";
+import { motion } from "framer-motion";
 
-export default function ProfileSearcher() {
-  const [foundedProfiles, setFoundedProfiles] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+export default function ProfileSearcher({ profiles }) {
+  const [searched, setSearched] = useState("");
+  const [founded, setFounded] = useState([]);
+  const sliderRef = useRef(null)
 
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setError(null);
-
-    try {
-      const response = await fetch(
-        `/api/getProfilesByName?name=${searchTerm}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            // Add authentication headers here if required
-            // 'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        const errorText = await response.text();
-        throw new Error(
-          `${response.status} ${response.statusText}: ${errorText}`
-        );
-      }
-
-      const data = await response.json();
-      setFoundedProfiles(data);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
+  useEffect(() => {
+    if (searched != "") {
+      setFounded(profiles.filter((p) => p.username.includes(searched)));
+    } else {
+      setFounded([]);
     }
-  };
+  }, [searched, profiles]);
 
   return (
     <>
-      <form
-        onSubmit={handleFormSubmit}
-        className="w-full md:max-w-[500px] mx-auto flex items-center justify-between overflow-hidden outline-none border-2 rounded-full"
-      >
+      <form className="w-full md:max-w-[500px] mx-auto flex items-center justify-between overflow-hidden outline-none border-2 rounded-full px-4">
+        <i className="text-2xl ri-search-line"></i>
         <input
           className="w-full bg-transparent outline-none px-4 py-2"
           type="text"
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={(e) => setSearched(e.target.value)}
           placeholder="Search profiles"
         />
-        <button
-          type="submit"
-          className="flex flex-nowrap items-center justify-center px-4 py-1 text-indigo-800 bg-white"
-        >
-          <i className="text-2xl ri-search-line"></i>Search
-        </button>
       </form>
-      {loading && <div className="mt-4">Loading...</div>}
-      {error && <div className="mt-4">Error: {error}</div>}
-      {foundedProfiles && foundedProfiles.length > 0 && (
-        <ul className="min-h-16 w-full flex items-center justify-start mt-4">
-          {foundedProfiles.map((profile) => (
-            <li key={profile.id}>{profile.username}</li>
+      <div
+          className="w-fit max-w-full"
+          ref={sliderRef}
+        >
+      <motion.ul
+        drag="x"
+        dragConstraints={sliderRef}
+        className="w-fit flex items-center justify-start mt-4 gap-8"
+      >
+        {profiles &&
+          founded.length > 0 &&
+          founded.map((profile) => (
+            <li key={profile.id}>
+              <ProfileCard profile={profile} />
+            </li>
           ))}
-        </ul>
-      )}
+      </motion.ul></div>
     </>
   );
 }
