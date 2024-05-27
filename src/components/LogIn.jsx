3ecb5f27cd2a796f819e240360cf848a517fbd08
AlonsoMartinez8 <pameprojectmail@gmail.com>
2024-05-27 15:36:18 +0200

    Login checks

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,40 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function LogIn() {
+  const [usernameValue, setUsernameValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+  const [warning, setWarning] = useState({ warn: false, msg: "" });
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        `/api/login?password=${passwordValue}&username=${usernameValue}`,
+        {
+          method: "POST",
+        }
+      );
+      const data = await response.json();
+      if (response.status !== 200) {
+        setWarning({ warn: true, msg: data.error || "Invalid credentials" });
+      } else {
+        setWarning({ warn: false, msg: "" });
+        // Redirect user to profile or another appropriate page
+        window.location.href = "/profile";
+      }
+    } catch (err) {
+      console.error(err);
+      setWarning({ warn: true, msg: "An error occurred. Please try again." });
+    }
+  };
+
   return (
-    <main class="h-full w-full flex flex-col items-center justify-between pb-4">
+    <main className="h-full w-full flex flex-col items-center justify-between pb-4">
       <form
-        class="w-full min-w-[300px] max-w-[500px] mx-auto flex flex-col gap-4 p-4"
-        method="POST"
-        action="api/login"
+        className="w-full min-w-[300px] max-w-[500px] mx-auto flex flex-col gap-4 p-4"
+        onSubmit={handleFormSubmit}
       >
-        <h1 class="text-center text-xl">Log Into Your Account</h1>
-        <label for="username">
+        <h1 className="text-center text-xl">Log Into Your Account</h1>
+        <label htmlFor="username">
           <p>User Name</p>
           <input
-            class="w-full bg-transparent border-2 mt-2 py-1 px-2 rounded-xl"
+            className="w-full bg-transparent border-2 mt-2 py-1 px-2 rounded-xl"
             type="text"
             name="username"
             placeholder="User name"
+            onChange={(e) => setUsernameValue(e.target.value)}
           />
         </label>
-        <label for="password">
+        <label htmlFor="password">
           <p>Password</p>
           <input
-            class="w-full bg-transparent border-2 mt-2 py-1 px-2 rounded-xl"
+            className="w-full bg-transparent border-2 mt-2 py-1 px-2 rounded-xl"
             type="password"
             name="password"
             placeholder="Password"
+            onChange={(e) => setPasswordValue(e.target.value)}
           />
         </label>
-        <button class="bg-white/20 mt-2 py-1 rounded-xl hover:shadow-md hover:shadow-white/10">
+        {warning.warn && <p className="text-xs text-red-400">{warning.msg}</p>}
+        <button className="bg-white/20 mt-2 py-1 rounded-xl hover:shadow-md hover:shadow-white/10">
           Log In
         </button>
       </form>
-      <footer class="text-center mb-4">
-        <p class="opacity-80">Don't have an account yet?</p>
-        <a class="text-lg py-1 border-b-[1px] animate-pulse" href="/register">
-          Register a new <span class="font-semibold">P A M E</span> account
+      <footer className="text-center mb-4">
+        <p className="opacity-80">Don't have an account yet?</p>
+        <a
+          className="text-lg py-1 border-b-[1px] animate-pulse"
+          href="/register"
+        >
+          Register a new <span className="font-semibold">P A M E</span> account
         </a>
       </footer>
     </main>
--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,51 +1,66 @@
 import type { APIContext } from "astro";
 import { User, db, eq } from "astro:db";
-import { generateId } from "lucia";
 import { Argon2id } from "oslo/password";
 import { lucia } from "@/auth";
 
 export async function POST(context: APIContext): Promise<Response> {
-  // Form Data
-  const formData = await context.request.formData();
-  const username = formData.get("username");
-  const password = formData.get("password");
-  // Validate Data
-  if (!username || !password) {
-    return new Response("All field are required", { status: 400 });
+  try {
+    const url = new URL(context.request.url);
+    const password = url.searchParams.get("password");
+    const username = url.searchParams.get("username");
+
+    // Validate Data
+    if (
+      !username ||
+      !password ||
+      typeof username !== "string" ||
+      typeof password !== "string"
+    ) {
+      return new Response(JSON.stringify({ error: "Invalid credentials" }), {
+        status: 400,
+      });
+    }
+
+    // Search User in DB
+    const foundUser = (
+      await db.select().from(User).where(eq(User.username, username))
+    ).at(0);
+
+    // User not found
+    if (!foundUser) {
+      return new Response(JSON.stringify({ error: "Invalid credentials" }), {
+        status: 400,
+      });
+    }
+
+    // Verify Password
+    const validPassword = await new Argon2id().verify(
+      foundUser.password,
+      password
+    );
+    if (!validPassword) {
+      return new Response(JSON.stringify({ error: "Invalid credentials" }), {
+        status: 400,
+      });
+    }
+
+    // Generate Session
+    const session = await lucia.createSession(foundUser.id, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    context.cookies.set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes
+    );
+
+    // Successfully created session and set cookie
+    return new Response(JSON.stringify({ success: true }), {
+      status: 200,
+    });
+  } catch (err) {
+    console.error(err);
+    return new Response(JSON.stringify({ error: "Internal server error" }), {
+      status: 500,
+    });
   }
-  if (typeof username !== "string") {
-    return new Response("Non valid username", { status: 400 });
-  }
-  if (typeof password !== "string") {
-    return new Response("Non valid password", { status: 400 });
-  }
-  // Search User in DB
-  const foundUser = (
-    await db.select().from(User).where(eq(User.username, username))
-  ).at(0);
-  // User not found
-  if (!foundUser) {
-    return new Response("Incorrect username or password", { status: 400 });
-  }
-  // Verify password
-  if (!password) {
-    return new Response("Incorrect username or password", { status: 400 });
-  }
-  const validPassword = await new Argon2id().verify(
-    foundUser.password,
-    password
-  );
-  if (!validPassword) {
-    return new Response("Incorrect username or password", { status: 400 });
-  }
-  // Generate Session
-  const session = await lucia.createSession(foundUser.id, {});
-  const sessionCookie = lucia.createSessionCookie(session.id);
-  context.cookies.set(
-    sessionCookie.name,
-    sessionCookie.value,
-    sessionCookie.attributes
-  );
-  // Redirect user
-  return context.redirect("/profile");
 }
